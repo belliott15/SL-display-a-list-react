0106f7aa7d24045f823a46e4a673a67eb4ed4328
Beau Elliott <belliott1516@gmail.com>
2022-04-28 12:40:48 -0700

    created plants, plantItem, and PlantList

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 import DipList from './DipList.js';
 import CandyList from './CandyList.js';
 import BookList from './BookList.js';
+import PlantList from './PlantList.js';
 import { dips } from './dips.js';
 import { candies } from './candies.js';
 import { books } from './books.js';
+import { plants } from './plants.js';
 import './App.css';
 // import your arrays here
 
@@ -18,6 +20,8 @@ function App() {
         <CandyList candies={candies} />
         <h1>Favorite Books</h1>
         <BookList books={books} />
+        <h1>Favorite Plants</h1>
+        <PlantList plants={plants} />
       </div>
     </div>
     
