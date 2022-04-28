838a025c9703ad71c845876d2c38adec92780e77
Beau Elliott <belliott1516@gmail.com>
2022-04-28 11:12:36 -0700

    added dips, diplist, and dipitem

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
+import DipList from './DipList.js';
 import CandyList from './CandyList.js';
-import './App.css';
+import { dips } from './dips.js';
 import { candies } from './candies.js';
+import './App.css';
 // import your arrays here
 
 function App() {
   return (
     <div>
       <div className="App">
+        <h1>Favorite Dips</h1>
+        <DipList dips={dips} />
+        <h1>Favorite Candy</h1>
         <CandyList candies={candies} />
+        <h1>Favorite Books</h1>
+        <BookList books={books} />
       </div>
     </div>
     
---
+++ src/DipItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DipItem({ dip }) {
+  return (
+    <div>
+      <p>{dip}</p>
+    </div>
+  );
+}
+
